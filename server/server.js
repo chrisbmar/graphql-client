4a317c16ea172788d62637560591259f445d8b63
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-06-03 11:23:58 +0200

    update mongolab referencies to mongodb

--- server/server.js
+++ server/server.js
@@ -9,14 +9,16 @@ const app = express();
 
 const MONGO_URI = process.env.DB_URI;
 if (!MONGO_URI) {
-  throw new Error("You must provide a MongoLab URI");
+  throw new Error("You must provide a MongoDB URI");
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-  .once("open", () => console.log("Connected to MongoLab instance."))
-  .on("error", (error) => console.log("Error connecting to MongoLab:", error));
+  .once("open", () => console.log("Connected to MongoDB Atlas instance."))
+  .on("error", (error) =>
+    console.log("Error connecting to MongoDB Atlas:", error)
+  );
 
 app.use(bodyParser.json());
 app.use(
