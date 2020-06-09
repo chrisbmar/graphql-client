cb7553ec830fab29fee8fd9c6940e32133c31538
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-06-09 16:16:56 +0200

    add song detail component

---
+++ client/components/SongDetail/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SongDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Song Detail</h1>
+      </div>
+    );
+  }
+}
+
+export default SongDetail;
--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ import { ApolloProvider } from "react-apollo";
 import App from "./components/App/index";
 import SongList from "./components/SongList/index";
 import SongCreate from "./components/SongCreate/index";
+import SongDetail from "./components/SongDetail/index";
 import "./style/style.css";
 
 const client = new ApolloClient({});
@@ -17,6 +18,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
           <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
